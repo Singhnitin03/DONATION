0c4fbda55a003fac7f7b1d0184b00f180d428d8f
NITIN KUMAR <49977766+Singhnitin03@users.noreply.github.com>
2020-09-25 12:36:58 +0530

    Add files via upload

---
+++ pay.php
@@ -0,0 +1,31 @@
+<?php
+
+    $Name=$_POST['Name'];
+    $Email=$_POST['Email'];
+    $Amount=$_POST['Amount'];
+    $Phone=$_POST['phone'];
+    $purpose='Donation';
+    
+    include 'instamojo/Instamojo.php';
+    $api = new Instamojo\Instamojo('test_d9c837f804055de2559c17da509', 'test_4a3206a87cddc8721ceacf81cc3', 'https://test.instamojo.com/api/1.1/');
+
+    try {
+        $response = $api->paymentRequestCreate(array(
+            "purpose" => $purpose,
+            "amount" => $Amount,
+            "send_email" => true,
+            "email" => $Email,
+            "buyer_name" =>$Name,
+            "phone"=>$Phone,
+            "send_sms" => true,
+            "allow_repeated_payments" =>false,
+            "redirect_url" => "https://included-sky.000webhostapp.com//redirect.html"
+            )
+        );
+        $pay_url=$response['longurl'];
+        header("location: $pay_url");
+    	}
+    	catch (Exception $e) {
+    	    print('Error: ' . $e->getMessage());
+    	}
+?>
